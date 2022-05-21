72c6131361c3f467d7ea4d6eed2d02a65e03b3ca
sm-jahangir <samraatjahangir@gmail.com>
2022-05-21 20:29:20 +0600

    Home page completed

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Review = ({ review }) => {
   return (
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <div class="card-body">
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
         <p>{review.review}</p>
         <div className="flex items-center">
-          <div class="avatar">
-            <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 mr-5 w-16">
+          <div className="avatar">
+            <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 mr-5 w-16">
               <img src={review.img} alt="This is Avatar" />
             </div>
           </div>
---
+++ src/Pages/Shared/Footer.js
@@ -0,0 +1,62 @@
+import React from "react";
+import footer from "../../assets/images/footer.png";
+const Footer = () => {
+  return (
+    <div
+      style={{
+        background: `url(${footer})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <footer className="footer p-10 flex justify-between px-48">
+        <div>
+          <span className="footer-title">Services</span>
+          <a href="/" className="link link-hover">
+            Branding
+          </a>
+          <a href="/" className="link link-hover">
+            Design
+          </a>
+          <a href="/" className="link link-hover">
+            Marketing
+          </a>
+          <a href="/" className="link link-hover">
+            Advertisement
+          </a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a href="/" className="link link-hover">
+            About us
+          </a>
+          <a href="/" className="link link-hover">
+            Contact
+          </a>
+          <a href="/" className="link link-hover">
+            Jobs
+          </a>
+          <a href="/" className="link link-hover">
+            Press kit
+          </a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a href="/" className="link link-hover">
+            Terms of use
+          </a>
+          <a href="/" className="link link-hover">
+            Privacy policy
+          </a>
+          <a href="/" className="link link-hover">
+            Cookie policy
+          </a>
+        </div>
+      </footer>
+      <div className="footer footer-center p-4 text-base-content">
+        <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
