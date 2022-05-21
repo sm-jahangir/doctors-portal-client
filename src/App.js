1bd0ff5a61b0b348245e9e8fe1370a4627238fd9
sm-jahangir <samraatjahangir@gmail.com>
2022-05-21 11:43:12 +0600

    all component created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from "./Pages/Shared/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
+import Appointment from "./Pages/Appointment/Appointment";
+import Contact from "./Pages/Contact/Contact";
+import Review from "./Pages/Review/Review";
+import Login from "./Pages/Auth/Login/Login";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
