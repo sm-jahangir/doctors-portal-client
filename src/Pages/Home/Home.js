93705ea6ccdd9d16c6f71dac75ac0d17e80682ab
sm-jahangir <samraatjahangir@gmail.com>
2022-05-21 18:30:18 +0600

    Treatment completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "./Banner";
 import Info from "./Info";
 import Services from "./Services";
+import Treatment from "./Treatment";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Banner />
       <Info />
       <Services />
+      <Treatment />
     </div>
   );
 }
---
+++ src/Pages/Home/Treatment.js
@@ -0,0 +1,30 @@
+import React from "react";
+import treatment from "../../assets/images/treatment.png";
+
+const Treatment = () => {
+  return (
+    <div className="card card-side bg-base-100 mx-48 border-0">
+      <figure>
+        <img className="rounded" src={treatment} alt="Movie" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-5xl">
+          Exceptional Dental Care, on Your Terms
+        </h2>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsumis that it has a more-or-less normal distribution of
+          letters,as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page
+        </p>
+        <div className="card-actions justify-start">
+          <button className="btn btn-primary">GET STARTED</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Treatment;
