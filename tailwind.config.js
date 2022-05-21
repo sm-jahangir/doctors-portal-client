71106fdee84f8e4587f8857097fcad493371fa99
sm-jahangir <samraatjahangir@gmail.com>
2022-05-21 11:21:12 +0600

    navbar implement and daisyui custom theme add

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,21 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        doctorstheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "white",
+      "cupcake",
+    ],
+  },
+
   plugins: [require("daisyui")],
 };
