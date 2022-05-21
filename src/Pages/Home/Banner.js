d2235673a6f81110fc25369d029ad4475d71c874
sm-jahangir <samraatjahangir@gmail.com>
2022-05-21 13:20:40 +0600

    Banner completed

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,27 @@
+import React from "react";
+import chair from "../../assets/images/chair.png";
+
+function Banner() {
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src={chair}
+          className="max-w-sm rounded-lg shadow-2xl"
+          alt="This is Banner"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+          <p className="py-6">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
