49e985c62169e413ba74cae8742a91f74a4078aa
sm-jahangir <samraatjahangir@gmail.com>
2022-05-22 12:18:08 +0600

    dynamic modal add

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
   const { name, slots } = service;
   return (
     <div className="card lg:max-w-lg bg-base-100 shadow-xl">
@@ -16,12 +16,14 @@ const Service = ({ service }) => {
           )}
         </p>
         <div className="card-actions justify-center">
-          <button
+          <label
+            for="booking-modal"
             disabled={slots.length === 0}
+            onClick={() => setTreatment(service)}
             className="btn btn-secondary text-white text-uppercase"
           >
             Book Appointment
-          </button>
+          </label>
         </div>
       </div>
     </div>
